687799b79863ff287e0d5aa754efd1246e1aaffd
Mayur <chaumayu@gmail.com>
2019-05-10 14:27:17 +0530

    fill missing values

--- Code/titanic.py
+++ Code/titanic.py
@@ -13,11 +13,30 @@ def Read(train,test):
 
     return train, test
 
+def FillMissingValues(df):
+    # Check for missing values
+    print df.isnull().any()
+
+    # Embarked feature has some missing values and fill most occurred value ('S').
+    print df['Embarked'].value_counts()
+    embarked = df['Embarked'].value_counts().index[0]
+    df['Embarked'] = df['Embarked'].fillna(embarked)
+
+    # Age feature has some missing values and fill mean of age
+    age_mean = int(df['Age'].mean())
+    # print age_mean
+    df['Age'] = df['Age'].fillna(age_mean)
+
+    print df.isnull().any()
+
 def main():
     train_path = "Data/titanic/train.csv"
     test_path = "Data/titanic/test.csv"
 
     train, test = Read(train_path, test_path)
+
+    # Train.csv
+    FillMissingValues(train)
 
 if __name__ == '__main__':
     main()
